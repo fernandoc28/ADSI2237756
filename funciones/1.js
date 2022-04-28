0c78700cd64040f4921d74e7569e3fd331ce9b19
fernandoc28 <fer.camacho.ur@gmail.com>
2022-04-28 15:43:46 -0500

    introduccion a funciones

--- funciones/1.js
+++ funciones/1.js
@@ -1,1 +1,8 @@
-console.log('Trabajo con funciones')
+const x=()=>"Hola";
+console.log(x())
+
+const y=(nombre)=>`Hola ${nombre}`;
+console.log (y("fernando"));
+
+const z=(nombre,apellido)=>`Hola ${nombre} ${apellido}`
+console.log(z("fernando","camacho"))ñ
